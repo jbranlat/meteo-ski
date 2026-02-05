c662a61a58a82c4d3b9d2e232aeac53ba9e54f24
jbranlat <jeremy.branlat@gmail.com>
2026-02-05 19:50:30 +0100

    correction PWA pour vercel

--- next.config.mjs
+++ next.config.mjs
@@ -5,11 +5,21 @@ const withPWA = withPWAInit({
   dest: 'public',
   register: true,
   skipWaiting: true,
-  disable: process.env.NODE_ENV === 'development', // Désactivé en développement
+  disable: process.env.NODE_ENV === 'development',
 });
 
 const nextConfig = {
   reactCompiler: true,
+  // Ajoute cette configuration pour forcer la compatibilité Webpack sur Vercel
+  webpack: (config) => {
+    return config;
+  },
+  // On indique explicitement à Turbopack de ne pas s'en mêler pour le moment
+  experimental: {
+    turbo: {
+      // Si tu as des règles spécifiques, elles iraient ici
+    }
+  }
 };
 
 export default withPWA(nextConfig);
